8ab12bb8cd74ec9b3f5e9e78119d7d95729d8c1c
cadullms <cadullms@github.com>
2020-08-14 00:10:34 +0200

    waiting for pods by label

--- waitforpodready.ps1
+++ waitforpodready.ps1
@@ -1,13 +1,35 @@
 function GetPodStatuses ($podName)
 {
-    $podJson = (kubectl get pod $podName -o json)
-    $podInfo = $podJson | ConvertFrom-Json
-    return $podInfo.status.containerStatuses
+    if ($podName -and $podLabel)
+    {
+        throw "Both podName AND podLabel are set, only one can be used."
+    }
+
+    if ($podName)
+    {
+        $podJson = (kubectl get pod $podName -o json)
+        $podInfo = $podJson | ConvertFrom-Json
+        return $podInfo.status.containerStatuses
+    }
+
+    if ($podLabel)
+    {
+        $podJson = (kubectl get pod -l $podLabel -o json)
+        $podInfo = $podJson | ConvertFrom-Json
+        return $podInfo.items.status.containerStatuses
+    }
+
+    throw "Either podName OR podLabel must be set."
 }
 
-function IsPodReady ($podName, $containerName = "")
+function IsPodReady ($podName, $podLabel, $containerName = "")
 {
-    $statuses = GetPodStatuses -podName $podName
+    $statuses = GetPodStatuses -podName $podName -podLabel $podLabel
+    if (-not $statuses)
+    {
+        return $false
+    }
+
     if ([String]::IsNullOrEmpty($containerName))
     {
         $totalCount = $statuses.Count
@@ -25,15 +47,26 @@ function IsPodReady ($podName, $containerName = "")
     }
 }
 
-function WaitForPodReady($podName, $containerName = "")
+function GetPodText($podName, $podLabel)
 {
-    Write-Output "Waiting for pod $podName to be ready..."
+    if ($podName)
+    {
+        return "pod $podName" 
+    }   
+
+    return "pod(s) with label $podLabel"
+}
+
+function WaitForPodReady($podName, $podLabel, $containerName = "")
+{
+    $podText = GetPodText -podName $podName -podLabel $podLabel
+    Write-Output "Waiting for $podText to be ready..."
     $interval = 0.25
     $maxInterval = 64
     $timeOut = 600
     $startTime = [DateTime]::Now
     $secondsPassed = 0
-    while (-not (IsPodReady -podName $podName -containerName $containerName))
+    while (-not (IsPodReady -podName $podName -podLabel $podLabel -containerName $containerName))
     {
         Start-Sleep -Seconds $interval
         if ($interval -le $maxInterval)
@@ -44,9 +77,9 @@ function WaitForPodReady($podName, $containerName = "")
         $secondsPassed = ([DateTime]::Now - $startTime).TotalSeconds
         if ($secondsPassed -gt $timeOut)
         {
-            throw "Timed out after $timeOut seconds waiting for pod $podName to get ready."
+            throw "Timed out after $timeOut seconds waiting for $podText to get ready."
         }
     }
     
-    Write-Output "Pod $podName ready after $secondsPassed seconds."
+    Write-Output "The $podText is ready after $secondsPassed seconds."
 }
--- waitforpodready.test.ps1
+++ waitforpodready.test.ps1
@@ -1,8 +1,16 @@
-. $PSScriptRoot\waitforpodready.ps1 # . means we are just loading the functions, not executing anything yet
+. $PSScriptRoot\waitforpodready.ps1 # The . at the start of this line means we are just loading the functions, not executing anything yet
+
+# WaitForPodReady -podLabel "run=notexist" # will timeout and throw
+# WaitForPodReady -podName "notexist" # will timeout and throw
+
+kubectl apply -f $PSScriptRoot\waiter.yaml 
+WaitForPodReady -podLabel "run=waiter" 
+kubectl delete pod -l "run=waiter"
+
 kubectl apply -f $PSScriptRoot\waiter.yaml 
 WaitForPodReady -podName waiterpod
-kubectl delete pod waiterpod
+kubectl delete pod -l "run=waiter"
 
 kubectl apply -f $PSScriptRoot\waiter.yaml 
 WaitForPodReady -podName waiterpod -containerName waitercontainer
-kubectl delete pod waiterpod
+kubectl delete pod -l "run=waiter"
